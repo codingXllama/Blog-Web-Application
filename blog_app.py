36695f183718d06a0803fe44a88d187129c38f5a
codingxllama <osamaalchalabi2@gmail.com>
2019-10-14 23:30:16 -0400

    Created a template folder with some html files

--- blog_app.py
+++ blog_app.py
@@ -1,16 +1,35 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
+
+# Creating a list of dictionary,where each dictionary is a post
+
+userPosts = [
+    {
+        "User": "Sam X",
+        "Content": "I love this pizza, give it a try!",
+        "title": "Nix Pizza Shop",
+        "postDate":" May 20, 1980"
+    },
+    {
+        "User": "Nuxa N",
+        "Content": "Wow this Ice-cream is so delicious",
+        "title": "Amazing Ice",
+        "postDate":"October 2,1998"
+    }
+]
+
 
 
 @app.route("/")
 def Home():
-    return "<h1>Welcome to the Home Page</h1>"
+    #sending the userposts as post
+    return render_template("home.html",posts=userPosts)
 
 #Routing to the About page
 @app.route("/about")
 def About():
-    return "Welcome to the About Page"
+    return render_template("about.html")
 
 
 #Running the app in Debug mode
