ed94deef8501046414aad4c455eb256c839a7693
codingxllama <osamaalchalabi2@gmail.com>
2019-10-14 23:44:15 -0400

    Created Dynamic Blocks

--- blog_app.py
+++ blog_app.py
@@ -1,6 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 # Creating a list of dictionary,where each dictionary is a post
 
@@ -9,30 +9,30 @@ userPosts = [
         "User": "Sam X",
         "Content": "I love this pizza, give it a try!",
         "title": "Nix Pizza Shop",
-        "postDate":" May 20, 1980"
+        "postDate": " May 20, 1980",
     },
     {
         "User": "Nuxa N",
         "Content": "Wow this Ice-cream is so delicious",
         "title": "Amazing Ice",
-        "postDate":"October 2,1998"
-    }
+        "postDate": "October 2,1998",
+    },
 ]
-
 
 
 @app.route("/")
 def Home():
-    #sending the userposts as post
-    return render_template("home.html",posts=userPosts)
+    # sending the userposts as post
+    return render_template("home.html", posts=userPosts)
 
-#Routing to the About page
+
+# Routing to the About page
 @app.route("/about")
 def About():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 
-#Running the app in Debug mode
+# Running the app in Debug mode
 
 if __name__ == "__main__":
     app.run(debug=True)
